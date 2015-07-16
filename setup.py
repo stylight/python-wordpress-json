9d851c4fe4b0b4753e7e3a56f88ee2d7acef2fa3
Raul Taranu <raul.taranu@stylight.com>
2015-07-16 15:04:23 +0200

    include HISTORY.rst in setup.py

--- setup.py
+++ setup.py
@@ -4,27 +4,31 @@
 import os
 import sys
 
-from setuptools import setup
+from codecs import open
+
+from setuptools import setup, find_packages
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-with open('README.rst', 'r') as f:
+with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
-with open('HISTORY.rst', 'r') as f:
+with open('HISTORY.rst', 'r', 'utf-8') as f:
     history = f.read()
 
 setup(
     name='wordpress_json',
-    version='0.1.4',
+    version='0.1.5',
     description='A thin wrapper for the Wordpress JSON API',
     long_description=readme + '\n\n' + history,
     author='Raul Taranu, Dimitar Roustchev',
     author_email='raul.taranu@stylight.com, dimitar.roustchev@stylight.com',
     url='http://github.com/stylight/python-wordpress-json',
     license='MIT License',
-    packages=['wordpress_json'],
+    packages=find_packages(),
+    package_data={'': ['*.rst']},
+    include_package_data=True,
     install_requires=[
         'requests',
     ],
